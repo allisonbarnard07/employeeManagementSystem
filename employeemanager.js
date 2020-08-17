68085670945d627259f6e554fa7ab6ab71f1f279
Allison Barnard <allisonbarnard07@gmail.com>
2020-08-17 14:17:52 -0400

    pushing updates

--- employeemanager.js
+++ employeemanager.js
@@ -20,7 +20,7 @@ connection.connect(function (err) {
     runSearch();
 });
 
-function runManager() {
+function runSearch() {
     inquirer.prompt({
         name: "action",
         type: "list",
@@ -41,25 +41,133 @@ function runManager() {
                     viewEmployees();
                     break;
 
-                case "View All Employees By Department":
+                case "View Departments":
                     viewEmployeeDepts();
                     break;
-                case "View All Employees By Role":
+                case "View Roles":
                     viewEmployeesRole();
                     break;
                 case "Add Employee":
                     addEmployee();
                     break;
-                case "Remove Employee":
-                    removeEmployee();
+                case "Add Role":
+                    addRole();
                     break;
+                    case "Add Department":
+                        addDepartment();
+                        break;
                 case "Update Employee Role":
                     updateRole();
                     break;
-                case "Update Employee Manager":
-                    updateManager();
-                    break;
+                    case 'Exit':
+                        connection.end();
+                        break;
 
             }
         });
-}
+    }
+
+    function viewEmployees(){
+var query = `SELECT * from employee`;
+connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    runSearch();
+  });
+    }
+
+    function viewEmployeesRole(){
+        var query = `SELECT * from role`;
+connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    runSearch();
+  });
+    }
+
+    function viewEmployeeDepts(){
+        var query = `SELECT * from department`;
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            runSearch();
+          });
+            }
+
+    function addEmployee(){
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: "Enter employee's first name",
+                name: 'first_name'
+            },
+            {
+                type: "input",
+                message: "Enter employee's last name",
+                name: "last_name"
+            }
+        ])
+        .then(function(answer) {
+            connection.query(
+              "INSERT INTO employee SET ?",
+              {
+                first_name: answer.firstname,
+                last_name: answer.lastname,
+              },
+              function(err, answer) {
+                if (err) {
+                  throw err;
+                }
+                console.table(answer);
+              }
+            );
+            runSearch();
+          });
+      }
+
+
+      function updateRole() {
+        var allEmployees = [];
+        connection.query("SELECT * FROM employee", function(err, answer) {
+
+          for (let i = 0; i < answer.length; i++) {
+            let employeeString =
+              answer[i].id + " " + answer[i].first_name + " " + answer[i].last_name;
+            allEmployees.push(employeeString);
+          }
+      
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "updateRole",
+                message: "Select an employee to their role",
+                choices: allEmployees
+              },
+              {
+                type: "list",
+                message: "Select a new role",
+                choices: ["manager", "employee"],
+                name: "newRole"
+              }
+            ])
+            .then(function(answer) {
+              console.log("Updating", answer);
+              const idUpdate = {};
+              idUpdate.id = parseInt(answer.updateRole.split(" ")[0]);
+              if (answer.newrole === "manager") {
+                idUpdate.role_id = 1;
+              } else if (answer.newrole === "employee") {
+                idUpdate.role_id = 2;
+              }
+              connection.query(
+                "UPDATE employee SET role_id = ? WHERE id = ?",
+                [idUpdate.role_id, idUpdate.id],
+                {
+                  runSearch();
+                }
+              );
+            });
+        });
+      }
+      
