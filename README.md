8de34d698684320f44a052aeb0e0dfb3523e47ef
Allison Barnard <allisonbarnard07@gmail.com>
2020-08-17 16:57:12 -0400

    Added README and mySQL updates

---
+++ README.md
@@ -0,0 +1,29 @@
+
+
+      ##Title 
+      Employee Management System App
+      
+      ##Description 
+      This app was created using MySQL and JS. It was created to help companies easily manage new employees. It also gives companies the ability to edit employees. Functionality includes: Ability to add employees, departments, and roles; View employees, departments and roles; and update employee roles. 
+
+      ## Testing
+       undefined
+
+      ## Table of Contents 
+      1. Instructions 2. Usage 3.Licenses 4.Contribution 5.Tests
+
+      ## Installation
+      Node.js is required to run this app      
+
+      ## Usage 
+      Installation of mySQL, inquirer and console.table is required for application useage
+
+      ## Licenses 
+      MIT
+
+      ## Contribution 
+      Allison Barnard
+
+      ## Testing 
+      N/A
+      
--- employees_db.sql
+++ employees_db.sql
@@ -5,23 +5,54 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-id INT NOT NULL auto_increment,
-name varchar(30) NOT NULL,
-primary key (id)
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+dept_name varchar(30) NOT NULL
 );
 
 CREATE TABLE role (
-id INT NOT NULL auto_increment,
+id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
 title VARCHAR(30) NOT NULL,
-salary decimal NOT NULL,
-department_id INT NOT NULL auto_increment,
-primary key (id)
+salary decimal(8, 2) NOT NULL,
+department_id INT NOT NULL,
+FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-id INT NOT NULL auto_increment,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id INT NOT NULL auto_increment,
-manager_id INT
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT, 
+FOREIGN KEY(role_id) REFERENCES role(id),
+FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
+
+SELECT * from department;
+
+INSERT INTO department
+    (dept_name)
+VALUES
+    ('Manager'),
+    ('Employee');
+    
+    INSERT INTO role
+    (title, salary, department_id)
+VALUES
+	('Manager', 120000, 1),
+	('Employee', 50000, 2);
+
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+('Taz', 'Barnard', 1, 1),
+('Brit', 'Grin', 1, 1),
+('King', 'Steven', 2, 2),
+('Emmitt', 'Smith', 2, 2);
+
+select * from department;
+select * from role; 
+select * from employee; 
+
+
+
